fd1434d7ca13c3b44147adc8be517ee61ef76fe1
Flávio de Oliveira Campos <arq.flaviocampos@gmail.com>
2026-05-10 22:56:26 -0300

    Update script.js

--- unidade_3/script.js
+++ unidade_3/script.js
@@ -1,1 +1,44 @@
+function pegarLocalizacao(){
 
+navigator.geolocation.getCurrentPosition(
+    
+    (posicao)=>{
+
+        const latitude = posicao.coords.latitude;
+        const longitude = posicao.coords.longitude;
+
+        document.getElementById("status").innerHTML = `
+        
+        Latitude: ${latitude}
+        <br>
+        Longitude: ${longitude}
+        
+        `;
+
+        gerarLink(latitude, longitude);
+
+    },
+
+    ()=>{
+
+        alert("Não foi possível pegar localização");
+
+    }
+
+);
+
+}
+
+function gerarLink(lat, long){
+
+const link = `
+aluno.html?lat=${lat}&long=${long}
+`;
+
+document.getElementById("linkGerado").innerHTML = `
+<a href="${link}">
+${link}
+</a>
+`;
+
+}
