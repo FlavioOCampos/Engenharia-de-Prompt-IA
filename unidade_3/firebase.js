ba8112e7c09160e26927e2b4c9024911fbed5a20
Flávio de Oliveira Campos <arq.flaviocampos@gmail.com>
2026-05-11 00:29:47 -0300

    Update firebase.js

--- unidade_3/firebase.js
+++ unidade_3/firebase.js
@@ -1,1 +1,28 @@
+// Importações Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Configuração Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCQXNXcAwQxnrfp_LXyMZDYHQGFIgBhlY",
+  authDomain: "geopresenceai.firebaseapp.com",
+  projectId: "geopresenceai",
+  storageBucket: "geopresenceai.firebasestorage.app",
+  messagingSenderId: "50359090351",
+  appId: "1:50359090351:web:baee622c4487d20147685a"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Banco de dados
+const db = getFirestore(app);
+
+// Exportar banco
+export { db, collection, addDoc, getDocs };
